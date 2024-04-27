8b3309b8f0f011a6a2db5ac133540d0b6fe0ec10
DhewaRadya <radyadhewa@gmail.com>
2024-04-27 23:59:00 +0700

    finish attraction api

--- backend/attractions/index.js
+++ backend/attractions/index.js
@@ -4,6 +4,7 @@ const PORT = 5000;
 var admin = require("firebase-admin");
 const serviceAccount = require("./key.json");
 
+// init app and credential
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://fir-login-24502-default-rtdb.firebaseio.com",
@@ -11,8 +12,10 @@ admin.initializeApp({
 
 const db = admin.database();
 
+// middleware json
 app.use(express.json())
 
+// get all
 app.get('/attractions', (req, res) => {
   db.ref('/attractions').once('value', snapshot => {
     const attractions = [];
@@ -25,6 +28,7 @@ app.get('/attractions', (req, res) => {
   });
 });
 
+// post and put
 app.post('/attractions/:id', (req, res) => {
   const { id } = req.params;
   const { name, description, type, location, rating, imageUrl } = req.body;
@@ -49,9 +53,51 @@ app.post('/attractions/:id', (req, res) => {
   });
 });
 
+// delete
+app.delete('/attractions/:id', (req, res) => {
+  const { id } = req.params;
 
+  const attractionRef = db.ref(`/attractions/${id}`);
 
+  attractionRef.remove()
+    .then(() => {
+      console.log("Attraction deleted successfully");
+      res.status(200).json({ message: "Attraction deleted successfully" });
+    })
+    .catch(error => {
+      console.error("Error deleting attraction:", error);
+      res.status(500).json({ error: "Error deleting attraction" });
+    });
+});
 
+// filtered get
+app.get('/filterattractions', (req, res) => {
+  const location = req.query.location;
+
+  if (location) {
+    db.ref('/attractions').orderByChild('location').equalTo(location).once('value', snapshot => {
+      const attractions = [];
+      snapshot.forEach(childSnapshot => {
+        const attractionId = childSnapshot.key;
+        const attractionData = childSnapshot.val();
+        attractions.push({ id: attractionId, ...attractionData });
+      });
+      res.status(200).json(attractions);
+    });
+  } else {
+    db.ref('/attractions').once('value', snapshot => {
+      const attractions = [];
+      snapshot.forEach(childSnapshot => {
+        const attractionId = childSnapshot.key;
+        const attractionData = childSnapshot.val();
+        attractions.push({ id: attractionId, ...attractionData });
+      });
+      res.status(200).json(attractions);
+    });
+  }
+});
+
+// app server
 app.listen(
   PORT,
   () => console.log(`It's alive on http://localhost:${PORT}`)
