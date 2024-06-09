6515600368f2a42655cea5e55e5db251444e4dd5
DhewaRadya <radyadhewa@gmail.com>
2024-06-09 22:08:46 +0700

    docs: add yaml for swagger

--- touristApp/api/articles/app.py
+++ touristApp/api/articles/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_cors import CORS
 import os
 
 # app 
 app = Flask(__name__)
+CORS(app)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # database
